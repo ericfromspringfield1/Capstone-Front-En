0c2d73de7fc6c7905f1579646b040826c8db3ecf
Eric Taylor <ericfromspringfield@gmail.com>
2019-09-23 23:51:55 -0500

    Update GigsCard.js

--- components/Gigs/GigsCard.js
+++ components/Gigs/GigsCard.js
@@ -1,48 +1,51 @@
-  .card {
-    /* Add shadows to create the "card" effect */
-    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-    transition: 0.3s;
-    border-radius: 5px; /* 5px rounded corners */
-    background: aliceblue;
-    margin-bottom: 2em;
-    flex: 0 1 24%;
-  }
-  
-  .card-content {
-    padding: .5em;
-  }
-  
-  .card-content h3 {
-    margin-top: 0;
-    margin-bottom: .5em;
-    font-weight: bold;
-    color: rgb(43, 112, 241);
-  }
-  
-  .card-content p {
-    font-size: 80%;
-    color: darkslategrey;
-  }
-  
-  .content-petname {
-    color: darkslategrey;
-  }
-  
-  .card button {
-    background-color: cornflowerblue;
-    border: none;
-    color: cornsilk;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 14px;
-    padding: .5em;
-    border-radius: 2px;
-    margin: .1em;
-    width: 100%;
-  }
-  
-  .card button:hover {
-    background-color: darkblue;
-  }
-  
+ import React, { Component } from 'react';
+import './Gigs.css'
+import { Link } from "react-router-dom";
+
+class GigsCard extends Component {
+  render() {
+    return (
+      <div className="card">
+          <h3><span className="gigName" style={{ color: 'darkslategrey' }}> {this.props.gig.name}</span></h3>
+          <h5 className="gigDate">{this.props.gig.date}</h5>
+          <h5 className="venueName">{this.props.gig.venue.name}</h5>
+          <h5 className="venueCity">{this.props.gig.venue.city}</h5>
+          <h5 className="venueState">{this.props.gig.venue.state}</h5>
+          <h5 className="venueCapacity">Venue Capacity: {this.props.gig.venue.capacity}</h5>
+          <h5 className="audienceName">The Audience Was A Buncha: {this.props.gig.audience.name}</h5> 
+          <h5 className="spotName">Spot Order: {this.props.gig.spot.name}</h5>
+          <picture>
+            <img src={require('./charlieChaplin.png')} alt="Chaplin" />
+          </picture>
+          <button type="button" onClick={() => {this.props.history.push(`/gigs/${this.props.gig.id}/edit`)}}>Edit</button>
+          <button type="button" onClick={() => this.props.deleteGig(this.props.gig.id)}>Delete Gig</button>
+          <Link to={`/gigs/${this.props.gig.id}`}><button>Details</button></Link>
+
+          </div>
+    );
+  } 
+}
+
+export default GigsCard; 
+
+// import React, { Component } from 'react';
+// import './Gigs.css'
+// import { Link } from "react-router-dom";
+
+// class GigsCard extends Component {
+//   render() {
+//     return (
+//       <div className="card">
+//           <h3> {this.props.gig.name}</h3> <h5>{this.props.gig.date}</h5>
+//           <picture>
+//             <img src={require('./charlieChaplin.png')} alt="Chaplin" />
+//           </picture>
+//           <button type="button" onClick={() => this.props.deleteGig(this.props.gig.id)}>Delete Gig</button>
+//           <button type="button" onClick={() => {this.props.history.push(`/gigs/${this.props.gig.id}/edit`)}}>Edit</button>
+//           <button type="button" onClick={() => { this.props.history.push(`/gigs/${this.props.gig.id}/gigs`) }}>Details</button>
+//           </div>
+//     );
+//   }
+// }
+
+// export default GigsCard;  
