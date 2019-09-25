4c71980b7510d7505864c25bd5c64f2d683779b1
Eric Taylor <ericfromspringfield@gmail.com>
2019-09-25 16:31:58 -0500

    Update VenuesForm.js

--- components/Venues/VenuesForm.js
+++ components/Venues/VenuesForm.js
@@ -1,99 +1,110 @@
-import React, { Component } from 'react';
-import VenuesManager from '../../modules/VenuesManager';
-import './VenuesForm.css'
+import React, { Component } from "react";
+import VenuesManager from "../../modules/VenuesManager";
+import "./VenuesForm.css";
 
 class VenuesForm extends Component {
-    state = {
-        venue: "",
-        city: "",
-        state: "",
-        capacity: "",
-        loadingStatus: false,
+  state = {
+    name: "",
+    city: "",
+    state: "",
+    capacity: "",
+    userId: "",
+    loadingStatus: false
+  };
+  
+  constructNewVenue = evt => {
+    evt.preventDefault();
+    if (this.state.name === "" || this.state.city === "" || this.state.state === "" ||
+   this.state.capacity === ""){
+    window.alert("Please Enter Data Into All Fields")
+   } else {
+    this.setState({ loadingStatus: true });
+    const venue = {
+      name: this.state.name,
+      city: this.state.city,
+      state: this.state.state,
+      capacity: this.state.capacity,
+      userId: parseInt(sessionStorage.getItem("credentials"))
     };
+    console.log(venue)
+    // Create the venue and redirect user to venue list
+    VenuesManager.post(venue).then(() => this.props.history.push("/venues"));
+  }
+  };
 
-    handleFieldChange = evt => {
-        const stateToChange = {};
-        stateToChange[evt.target.id] = evt.target.value;
-        this.setState(stateToChange);
-    };
+  componentDidMount() {
+       VenuesManager.getAll().then(venues =>
+       this.setState({ venues: venues })
+       );
+  }
 
-    /*  Local method for validation, set loadingStatus, create venue object, invoke the VenuesManager post method, and redirect to the full venue list
-    */
-    constructNewVenue = evt => {
-        evt.preventDefault();
-        if (this.state.venue === "" || this.state.city === "" || this.state.state === "") {
-            window.alert("Please input a venue, city, and state");
-        } else {
-            this.setState({ loadingStatus: true });
-            const venue = {
-                venue: this.state.venue,
-                city: this.state.city,
-                state: this.state.state,
-                state: this.state.capacity,
-                userId: parseInt(sessionStorage.getItem("credentials"))
-            };
+  handleFieldChange = evt => {
+    const stateToChange = {};
+    stateToChange[evt.target.id] = evt.target.value;
+    this.setState(stateToChange);
+  };
 
-            // Create the venue and redirect user to venue list
-            VenuesManager.post(venue)
-            .then(() => this.props.history.push("/venues"));
-        }
-    };
+  /*  Local method for validation, set loadingStatus, create venue object, invoke the VenuesManager post method, and redirect to the full venue list
+   */
 
-    render(){
+  render() {
+    return (
+      <>
+        <form>
+          <fieldset>
+            <div className="formgrid">
+              <input
+                type="text"
+                required
+                onChange={this.handleFieldChange}
+                id="name"
+                placeholder="Venue"
+              />
+              <label htmlFor="venue">Venue</label>
 
-        return(
-            <>
-            <form>
-                <fieldset>
-                    <div className="formgrid">
-                        <input
-                        type="text"
-                        required
-                        onChange={this.handleFieldChange}
-                        id="venue"
-                        placeholder="Venue"
-                        />
-                        <label htmlFor="venue">Venue</label>
-                        
-                        <input
-                        type="text"
-                        required
-                        onChange={this.handleFieldChange}
-                        id="city"
-                        placeholder="City"
-                        />
-                        <label htmlFor="city">City</label>
+              <input
+                type="text"
+                required
+                onChange={this.handleFieldChange}
+                id="city"
+                placeholder="City"
+              />
+              <label htmlFor="city">City</label>
 
-                        <input
-                        type="text"
-                        required
-                        onChange={this.handleFieldChange}
-                        id="state"
-                        placeholder="State"
-                        />
-                        <label htmlFor="state">State</label>
+              <input
+                type="text"
+                required
+                onChange={this.handleFieldChange}
+                id="state"
+                placeholder="State"
+              />
+              <label htmlFor="state">State</label>
 
-                        <input
-                        type="text"
-                        required
-                        onChange={this.handleFieldChange}
-                        id="capacity"
-                        placeholder="Capacity"
-                        />
-                        <label htmlFor="capacity">Capacity</label>
-                    </div>
-                    <div className="alignRight">
-                        <button
-                        type="button"
-                        disabled={this.state.loadingStatus}
-                        onClick={this.constructNewVenue}
-                        >Submit</button>
-                    </div>
-                </fieldset>
-            </form>
-        </>
-        )
-    }
+              <input
+                type="text"
+                required
+                onChange={this.handleFieldChange}
+                id="capacity"
+                placeholder="Capacity"
+              />
+              <label htmlFor="capacity">Capacity</label>
+            </div>
+
+            <div className="alignRight">
+              <button
+                type="button"
+                disabled={this.state.loadingStatus}
+                onClick={this.constructNewVenue}
+              >
+                Submit
+              </button>
+            </div>
+          </fieldset>
+        </form>
+      </>
+    );
+  }
 }
 
-export default VenuesForm
+export default VenuesForm;
+
