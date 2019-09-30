6f30f51e447f72a961048e6afc38f94467837967
Eric Taylor <ericfromspringfield@gmail.com>
2019-09-30 17:24:32 -0500

    Update Welcome.js

--- components/Authenitication/Welcome.js
+++ components/Authenitication/Welcome.js
@@ -6,19 +6,16 @@ import "../Capstone.css"
 class Welcome extends Component {
     render() {
         return (
-            <>
-                <div className="card">
-                    <h1 className ="center">Welcome to your standUp jyrny jyrnyL</h1>
-                    <p className ="center">Document The Funny</p>
-                </div>
+            <>  
                 <div className="welcome--btns">
-                    <Link to="/Registration"><button className="register--btn">Register New Account</button></Link>
-                    <Link to="/Login"><button className="login--btn">Login</button></Link>
+                    <Link to="/Registration"><button className="register--btn">register new account</button></Link>
+                    <Link to="/Login"><button className="login--btn">login</button></Link>
                 </div>
-
+                <div>
                 <picture>
-                    <img src={require('./charlieChaplin.png')} alt="Hee Hee" />
+                    <img src={require('./capstoneLogo2.png')} alt="Hee Hee" />
                 </picture>
+                </div>
             </>
         );
     }
